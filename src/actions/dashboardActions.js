411ea39cfb9f79bef2b7e184dd293e918f2c1681
Arun Prasad <arunprasad@Aruns-MacBook-Pro.local>
2017-04-20 21:44:40 +0530

    validation added

--- src/actions/dashboardActions.js
+++ src/actions/dashboardActions.js
@@ -1,4 +1,4 @@
-import * as types from '../constants/actionTypes';
+import * as types from '../actions/actionTypes';
 
 export function test() {
   return function (dispatch) {
--- src/components/common/TextInput.jsx
+++ src/components/common/TextInput.jsx
@@ -1,37 +1,87 @@
 import React, { Component } from 'react';
-
+import {testSpaces, validateEmail, validatePassword, validateMobile} from '../../helpers/'
 export default class TextInput extends Component {
   constructor(props){
     super(props);
     this.state = {
-      passwordVisible: false
+      'passwordVisible': false,
+      'showValidationError': false
     }
   }
-
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.showValidationError) {
+      this.setState({showValidationError: nextProps.showValidationError})
+    }
+  }
+  handleInputChange(e) {
+    const inputValue = e.target.value;
+    const {type, onChange, name, limitCharacters} = this.props;
+    if ((type == "email" && !validateEmail(inputValue)) || (type == "password" && !validatePassword(inputValue)) || (type == "mobile" && !validateMobile(inputValue))) {
+      return;
+    } else {
+      this.setState({showValidationError: false});
+    }
+    if (onChange) {
+      if(limitCharacters && inputValue.length>limitCharacters){
+        return;
+      } else{
+        onChange(inputValue, undefined, name);
+      }
+    }
+  }
+  handleOnBlur(e) {
+    const inputValue = e.target.value;
+    const {type, name} = this.props;
+    if (!testSpaces(inputValue)) {
+        this.setState({showValidationError: true});
+    } else {
+        if ((type == "email" && !validateEmail(inputValue)) || (type == "password" && !validatePassword(inputValue)) || (type == "mobile" && !validateMobile(inputValue))) {
+            this.setState({showValidationError: true});
+        } else {
+          this.setState({showValidationError: false});
+        }
+      }
+  }
   render() {
-    const {label, showError, isOTP, errorMessage, showPasswordImg, isNumber} = this.props;
+    const {label, isOTP, validationError, isNumber, type} = this.props;
     let inputClass = 'form-group ';
-    if (showError) {
+    if (this.state.showValidationError) {
       inputClass += 'error ';
     }
     if (isOTP) {
       inputClass += 'otp-input ';
     }
-    return showPasswordImg ?
+    return type === "password" ?
     (
       <div className={inputClass}>
-          <input type={this.state.passwordVisible ? 'text' : 'password'} className="form-control form-input" required/>
-          {showPasswordImg && <span className="input-icon" onClick={(e) => {e.preventDefault(); this.setState({'passwordVisible': !this.state.passwordVisible})}}>
+          <input
+            type={type == "password" ? this.state.passwordVisible ? "text" : "password" : type}
+            className="form-control form-input"
+            onBlur={this
+              .handleOnBlur
+              .bind(this)
+            }
+            onChange={(e) => this.handleInputChange(e, type)}
+            required/>
+          {type === "password" && <span className="input-icon" onClick={(e) => {e.preventDefault(); this.setState({'passwordVisible': !this.state.passwordVisible})}}>
             <img src="../images/eye.png" alt="" />
           </span>}
-          {showError && errorMessage && <span className="error-text">{errorMessage}</span>}
+          {this.state.showValidationError && validationError && <span className="error-text">{validationError}</span>}
           <label>{label}</label>
       </div>
     ) :
     (
       <div className={inputClass}>
-          <input type={isNumber ? 'number' : 'text'} className="form-control form-input" required/>
-          {showError && errorMessage && <span className="error-text">{errorMessage}</span>}
+          <input
+            type={type == 'email' ? 'text': type == 'mobile' ? 'number' : 'text'}
+            className="form-control form-input"
+            onBlur={this
+              .handleOnBlur
+              .bind(this)
+            }
+            onChange={(e) => this.handleInputChange(e, type)}
+            required/>
+          {this.state.showValidationError && validationError && <span className="error-text">{validationError}</span>}
           <label>{label}</label>
       </div>
     );
