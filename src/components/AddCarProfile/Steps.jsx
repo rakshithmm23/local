525de945aa480d73dfc1ec97b29e174365cee051
Arun Prasad <arun@cartoonmango.com>
2017-05-25 20:26:11 +0530

    button disabled added

--- src/components/AddCarProfile/Steps.jsx
+++ src/components/AddCarProfile/Steps.jsx
@@ -11,9 +11,11 @@ class Steps extends Component {
         this.state = {
             activeLogo: null,
             activeModel: null,
-            tab1: false,
-            tab2: false,
-            tab3: true,
+            manufacturerTabVisible: true,
+            modelTabVisible: false,
+            otherDetailsTabVisible: false,
+            modelTabIsUnlocked: false,
+            otherDetailsTabIsUnlocked: false,
             imageUploaded: [],
             submissionError: false
         };
@@ -37,65 +39,77 @@ onFieldChange(value, key, name) {
     }
   }
     activeLogo(name) {
-        this.setState({ activeLogo: name })
+        this.setState({ activeLogo: name, modelTabIsUnlocked: true})
     }
     activeModel(name) {
-        this.setState({ activeModel: name })
+        this.setState({ activeModel: name, otherDetailsTabIsUnlocked: true})
     }
     tabOpen(val) {
-        if (val == 'tab1') {
-            this.setState({ tab1: true, tab2: false, tab3: false })
-        } else if (val == 'tab2') {
-            this.setState({ tab1: false, tab2: true, tab3: false })
-        } else if (val == 'tab3') {
-            this.setState({ tab1: false, tab2: false, tab3: true })
+        if (val == 'manufacturerTabVisible') {
+            this.setState({ manufacturerTabVisible: true, modelTabVisible: false, otherDetailsTabVisible: false })
+        } else if (val == 'modelTabVisible' && this.state.modelTabIsUnlocked) {
+            this.setState({ manufacturerTabVisible: false, modelTabVisible: true, otherDetailsTabVisible: false })
+        } else if (val == 'otherDetailsTabVisible', this.state.otherDetailsTabIsUnlocked) {
+            this.setState({ manufacturerTabVisible: false, modelTabVisible: false, otherDetailsTabVisible: true })
         }
     }
-    
-    
+
+
 
 
     render() {
         const carList = [
             {
                 logo: '../../images/logo1.png',
-                name: 'Aston Martin 1'
+                name: 'Aston Martin 1',
+                manufacturerId: 1
             }, {
                 logo: '../../images/logo2.png',
-                name: 'Alfa Romeo 1'
+                name: 'Alfa Romeo 1',
+                manufacturerId: 2
             }, {
                 logo: '../../images/logo3.png',
-                name: 'Acura 1'
+                name: 'Acura 1',
+                manufacturerId: 3
             }, {
                 logo: '../../images/logo1.png',
-                name: 'Aston Martin 2'
+                name: 'Aston Martin 2',
+                manufacturerId: 4
             }, {
                 logo: '../../images/logo2.png',
-                name: 'Alfa Romeo 2'
+                name: 'Alfa Romeo 2',
+                manufacturerId: 5
             }, {
                 logo: '../../images/logo3.png',
-                name: 'Acura 2'
+                name: 'Acura 2',
+                manufacturerId: 6
             }, {
                 logo: '../../images/logo1.png',
-                name: 'Aston Martin 3'
+                name: 'Aston Martin 3',
+                manufacturerId: 7
             }, {
                 logo: '../../images/logo2.png',
-                name: 'Alfa Romeo 3'
+                name: 'Alfa Romeo 3',
+                manufacturerId: 8
             }, {
                 logo: '../../images/logo3.png',
-                name: 'Acura 3'
+                name: 'Acura 3',
+                manufacturerId: 9
             }
         ]
         const carModel = [
             {
                 logo: '../../images/logo1.png',
-                name: 'Aston Martin 1'
+                name: 'Aston Martin 1',
+                modalId: 1
             }, {
                 logo: '../../images/logo2.png',
-                name: 'Alfa Romeo 1'
+                name: 'Alfa Romeo 1',
+                modalId: 2
             }, {
                 logo: '../../images/logo3.png',
-                name: 'Acura 1'
+                name: 'Acura 1',
+                modalId: 3
             }
         ]
         const carListView = map(carList, (carItem) => {
@@ -123,14 +137,14 @@ onFieldChange(value, key, name) {
         return (
             <div>
                 <section className="s1" >
-                    <div className="title" onClick={() => { this.tabOpen('tab1') }}>
+                    <div className="title" onClick={() => { this.tabOpen('manufacturerTabVisible') }}>
                         <h4>Step 1: Select The Manufacturer</h4>
-                        <i className={this.state.tab1 ? "mdi mdi-chevron-up" : "mdi mdi-chevron-down"} ></i>
+                        <i className={this.state.manufacturerTabVisible ? "mdi mdi-chevron-up" : "mdi mdi-chevron-down"} ></i>
                     </div>
-                    {this.state.tab1 && <div>
+                    {this.state.manufacturerTabVisible && <div>
                         <div className="search-box ">
                             <div className="remove-left-padding">
-                                <TextInput label="Car Profile Name" name="text" type="text"/>
+                                <TextInput label="Search for manufacturer" name="text" type="text"/>
                             </div>
                             <i className="mdi mdi-magnify"></i>
                         </div>
@@ -138,19 +152,19 @@ onFieldChange(value, key, name) {
                             {carListView}
                     </div>
                         <div className="next-button">
-                            <Button btnType="submit" btnSize="sm" fontSize={13} label="Next" btnCallBack={(e) =>{e.preventDefault(); this.tabOpen('tab2')}}/>
+                            <Button disabled={this.state.activeLogo ? false : true} btnType="submit" btnSize="sm" fontSize={13} label="Next" btnCallBack={(e) =>{e.preventDefault(); this.tabOpen('modelTabVisible'); this.setState({'modelTabIsUnlocked': this.state.activeLogo ? true : false})}}/>
                         </div>
 
                     </div>}
                 </section>
                 <section className="s2" >
-                    <div className="title" onClick={() => {this.tabOpen('tab2') }}>
-                        <h4>Step 2: Select The Modal</h4>
-                        <i className={this.state.tab2 ? "mdi mdi-chevron-up" : "mdi mdi-chevron-down"} ></i>
+                    <div className="title" onClick={() => {this.tabOpen('modelTabVisible') }}>
+                        <h4>Step 2: Select The model</h4>
+                        <i className={this.state.modelTabVisible ? "mdi mdi-chevron-up" : "mdi mdi-chevron-down"} ></i>
                     </div>
-                    {this.state.tab2 && <div>
+                    {this.state.modelTabVisible && <div>
                         <div className="container-fluid select-option">
-                            <div className="modal-select col-md-6">
+                            <div className="model-select col-md-6">
                                 <select>
                                     <option value="volvo">Volvo</option>
                                     <option value="saab">Saab</option>
@@ -164,16 +178,16 @@ onFieldChange(value, key, name) {
                             {carModelView}
                         </div>
                         <div className="next-button">
-                            <Button btnType="submit" btnSize="sm" fontSize={13} label="Next" btnCallBack={(e) =>{e.preventDefault(); this.tabOpen('tab3')}}/>
+                            <Button disabled={this.state.activeModel ? false : true} btnType="submit" btnSize="sm" fontSize={13} label="Next" btnCallBack={(e) =>{e.preventDefault(); this.tabOpen('otherDetailsTabVisible'); this.setState({'otherDetailsTabIsUnlocked': this.state.activeModel ? true : false})}}/>
                         </div>
                     </div>}
                 </section>
                 <section className="s3" >
-                    <div className="title" onClick={() => { this.tabOpen('tab3') }}>
+                    <div className="title" onClick={() => { this.tabOpen('otherDetailsTabVisible') }}>
                         <h4>Step 3: Enter Other Details</h4>
-                        <i className={this.state.tab3 ? "mdi mdi-chevron-up" : "mdi mdi-chevron-down"} ></i>
+                        <i className={this.state.otherDetailsTabVisible ? "mdi mdi-chevron-up" : "mdi mdi-chevron-down"} ></i>
                     </div>
-                    {this.state.tab3 &&
+                    {this.state.otherDetailsTabVisible &&
                         <div className="wrapper">
                             <div className="upload-image">
                                 <h4>upload images</h4>
@@ -182,40 +196,40 @@ onFieldChange(value, key, name) {
                             <div className="car-profile">
                                 <div className="container-fluid">
                                     <h4>car profile</h4>
-                                    
+
                                     <div className="col-md-6 remove-left-padding">
-                                        <TextInput label="Car Profile Name" name="text" type="text"  showValidationError={this.errors['text']}  validationError="Enter a valid text" onChange={this.onFieldChange.bind(this)}/>
+                                        <TextInput label="Car Profile Name" name="text" type="text"  showValidationError={this.errors['text']}  validationError="Profile Name cannot be empty" onChange={this.onFieldChange.bind(this)}/>
                                     </div>
                                      <div className="col-md-6 remove-left-padding">
-                                        <TextInput label="Plate Number*" name="text" type="text" validationError="Enter a valid text"/>
+                                        <TextInput label="Plate Number*" name="text" type="text" validationError="Plate Number cannot be empty"/>
                                     </div>
                                      <div className="col-md-6 remove-left-padding">
-                                        <TextInput label="Kms Travelled*" name="text" type="text" validationError="Enter a valid text"/>
+                                        <TextInput label="Kms Travelled*" name="text" type="text" validationError="Kms Travelled cannot be empty"/>
                                     </div>
-                                    
+
                                 </div>
 
                             </div>
                             <div className="insurance-details">
                                 <div className="container-fluid">
-                                    <h4>insurance details</h4>
+                                    <h4>Insurance Details (Optional)</h4>
                                     <div className="col-md-6 remove-left-padding">
-                                        <TextInput label="Car Profile Name" name="text" type="text"  showValidationError={this.errors['text']}  validationError="Enter a valid text" onChange={this.onFieldChange.bind(this)}/>
+                                        <TextInput label="Insurance Provider" name="text" type="text" />
                                     </div>
                                      <div className="col-md-6 remove-left-padding">
-                                        <TextInput label="Plate Number*" name="text" type="text" validationError="Enter a valid text"/>
+                                        <TextInput label="Insurance Policy Number" name="text" type="text"/>
                                     </div>
                                      <div className="col-md-6 remove-left-padding">
-                                        <TextInput label="Kms Travelled*" name="text" type="text" validationError="Enter a valid text"/>
+                                        <TextInput label="State" name="text" type="text" />
                                     </div>
                                 </div>
 
                             </div>
                             <div className="car-notes">
                                 <div className="container-fluid">
-                                    <h4>insurance details</h4>
+                                    <h4>Car Notes (Optional)</h4>
                                      <div className="col-md-6 remove-left-padding">
-                                        <TextInput label="Kms Travelled*" name="text" type="text" validationError="Enter a valid text"/>
+                                        <TextInput label="Additional Details About The Car (Optional)" name="text" type="text" validationError="Enter a valid text"/>
                                     </div>
                                 </div>
                             </div>
