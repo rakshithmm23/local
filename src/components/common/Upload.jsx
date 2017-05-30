c66c4aa4d798ea7ac8ba724fd45220ce015c49a5
acamogh <acamogh@gmail.com>
2017-05-30 19:41:01 +0530

    upload changes

--- src/components/common/Upload.jsx
+++ src/components/common/Upload.jsx
@@ -25,7 +25,8 @@ class Upload extends Component {
         })
     }
     render() {
-        const imageUploadedView = this.props.page=="carProfile"? map(this.state.imageUploaded, (img,index) => {
+        let className = "upload-box-wrapper "+this.props.responsiveSize;
+        const imageUploadedView = map(this.state.imageUploaded, (img,index) => {
             return (
                 <div className={className}>
                     <div className="uploaded-image">
@@ -34,23 +35,22 @@ class Upload extends Component {
                         </span>
                         <img src={img.path} />
                     </div>
-                    {/*<h5>{img.name}</h5>*/} 
+                    {/*<h5>{img.name}</h5>*/}
                 </div>
             )
-        }):null
-        let className = "upload-box-wrapper "+this.props.responsiveSize;
-        
+        })
         return (
             <div className="img-uploads">
-                {this.props.page=="carProfile" &&  <div className="clearfix"> 
-                    <div className={className}>
-                        <label htmlFor="file-input" className="upload-box">
-                            <img src="../../images/attach.png" />
-                        </label>
-                        <input id="file-input" type="file" className="hide" onChange={(e) => this.fileNameUpload(e)} multiple />
-                    </div>
-                    {imageUploadedView}
-                </div>}
+                <div className="clearfix"> 
+                 {imageUploadedView}
+            
+                <div className={className}>
+                    <label htmlFor="file-input" className="upload-box">
+                        <img src="../../images/attach.png" />
+                    </label>
+                    <input id="file-input" type="file" className="hide" onChange={(e) => this.fileNameUpload(e)} multiple />
+                </div>
+                </div>
              </div>
         );
     }
