0b39f904d840f9a941c8d20b3a9370246b64853b
Bala Subramani <bala@cartoonmango.com>
2017-06-13 16:17:08 +0530

    authentication page changes doing

--- src/components/common/LoginHeader.jsx
+++ src/components/common/LoginHeader.jsx
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import Dropdown from '../common/Dropdown';
+import Search from '../common/Search';
+import { DropdownButton, MenuItem, Media } from 'react-bootstrap';
+import { map } from 'lodash';
 
-export default class LoginHeader extends Component {
-
+export default class Header extends Component {
     render() {
-        const {headerTitle} = this.props;
+      const {headerClickAction, pageType} = this.props;
         return (
-            <div className="login-title mobile">
-                <a href="index.html">
-                    <img src="../../images/back-arrow.png" alt="back" />
-                </a>
-                {headerTitle}
+            <div className="header-section navbar-fixed-top">
+                <div className="logo-section">
+                    <img src="../../images/logo-new.png" alt="" className="logo-img" />
+                </div>
+                <div className="header-right authNav-right">
+                    <ul className="list-unstyled">
+                        {pageType == "signIn" && <li>
+                           <label>
+                            Don’t have an account?
+                              <a href="" onClick={headerClickAction}>Sign Up</a>
+                           </label>
+                        </li>}
+                        {pageType == "signUp" && <li>
+                           <label>
+                            Already have an account?
+                              <a href="" onClick={headerClickAction}>Sign In</a>
+                           </label>
+                        </li>}
+                    </ul>
+                </div>
             </div>
         );
     }
