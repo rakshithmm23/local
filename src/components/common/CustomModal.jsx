f985c1aed71db4dce519a5501693c99ce502fb0b
acamogh <acamogh@gmail.com>
2017-06-30 11:01:55 +0530

    modal changes

--- src/components/common/CustomModal.jsx
+++ src/components/common/CustomModal.jsx
@@ -13,30 +13,27 @@ class CustomModal extends Component {
         // }    
     }
     componentWillReceiveProps(nextProps) {
-        // const { showModal } = nextProps;
-        // if(this.state.showModal != showModal && showModal != undefined) {
-        //     this.setState({
-        //         showModal: nextProps
-        //     });
-        // }
+        const { showModal } = nextProps;
+        if(this.state.showModal != showModal && showModal != undefined) {
+            this.setState({
+                showModal: nextProps
+            });
+        }
     }    
-    // close() {
-    //     // this.setState({ showModal: false });
-    //     if (this.props.onHide) {
-    //         this.props.onHide(this);
-    //     }
-    // }
+    close() {
+        this.setState({ showModal: false });
+    }
     render() {
-        const { children, footer, className, showModal } = this.props;
+        const { children, footer, className } = this.props;
         return (            
             <div>
-                <Modal className={className} show={showModal} onHide={() => this.props.onHide ? this.props.onHide() : ''}>
+                <Modal className={className} show={this.state.showModal} onHide={this.close.bind(this)}>
                     <Modal.Header closeButton>
                         <Modal.Title>{this.props.title}</Modal.Title>
                     </Modal.Header>
                     {children}
                     {footer=="true" && <Modal.Footer>
-                        <Button btnType="cancel" btnSize="sm" fontSize={15} label={this.props.cancelText?this.props.cancelText:"Cancel"}  btnCallBack={() => this.props.onHide ? this.props.onHide() : ''}/>
+                        <Button btnType="cancel" btnSize="sm" fontSize={15} label={this.props.cancelText?this.props.cancelText:"Cancel"}  btnCallBack={this.close.bind(this)}/>
                         <Button btnType="submit" btnSize="sm" fontSize={15} backgroundColor="red" label={this.props.saveText?this.props.saveText:"Save"} />
                     </Modal.Footer>}
                 </Modal>
