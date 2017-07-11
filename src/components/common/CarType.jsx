69d5b271cf7557e28ff4f07408a0d3afd9931648
acamogh <acamogh@gmail.com>
2017-07-11 16:02:29 +0530

    rating modal

--- src/components/common/CarType.jsx
+++ src/components/common/CarType.jsx
@@ -3,6 +3,7 @@ import { map } from 'lodash';
 import StatusBar from '../common/StatusBar';
 import CustomModal from '../common/CustomModal';
 import { DropdownButton, MenuItem, Modal, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
+import Rating from 'react-rating';
 
 class CardType extends Component {
     constructor() {
@@ -35,7 +36,7 @@ class CardType extends Component {
             return (
                 <div className="job-icon  notification" onClick={() => this.setState({ saveModal: true })}>
                     <span className="mdi mdi-star-outline"></span>
-                    <span className="commentLabel">Reviewwww</span>
+                    <span className="commentLabel">Review</span>
                 </div>
             );
         } else if (val == "cancelled") {
@@ -174,7 +175,7 @@ class CardType extends Component {
                     </div>
                 </div>}
 
-                <CustomModal footer="true" showModal={this.state.saveModal} className="rating-modal" closeIcon="true">
+                <CustomModal footer="true" showModal={this.state.saveModal} className="rating-modal" closeIcon="true" saveText="Submit">
                     <Modal.Body>
                         <div className="image-holder">
                             <img src="../../images/test.jpg" alt="" />
@@ -183,22 +184,12 @@ class CardType extends Component {
                             <span className="sub-text">120 AED</span>
                         </div>
                         <div className="stars">
-                                <div className="star-container ">
-                                    <fieldset className="rate">
-                                        <input type="radio" id="rating10" name="rating" value="10" /><label htmlFor="rating10" title="5 stars"></label>
-                                        <input type="radio" id="rating9" name="rating" value="9" /><label className="half" htmlFor="rating9" title="4 1/2 stars"></label>
-                                        <input type="radio" id="rating8" name="rating" value="8" /><label htmlFor="rating8" title="4 stars"></label>
-                                        <input type="radio" id="rating7" name="rating" value="7" /><label className="half" htmlFor="rating7" title="3 1/2 stars"></label>
-                                        <input type="radio" id="rating6" name="rating" value="6" /><label htmlFor="rating6" title="3 stars"></label>
-                                        <input type="radio" id="rating5" name="rating" value="5" /><label className="half" htmlFor="rating5" title="2 1/2 stars"></label>
-                                        <input type="radio" id="rating4" name="rating" value="4" /><label htmlFor="rating4" title="2 stars"></label>
-                                        <input type="radio" id="rating3" name="rating" value="3" /><label className="half" htmlFor="rating3" title="1 1/2 stars"></label>
-                                        <input type="radio" id="rating2" name="rating" value="2" /><label htmlFor="rating2" title="1 star"></label>
-                                        <input type="radio" id="rating1" name="rating" value="1" /><label className="half" htmlFor="rating1" title="1/2 star"></label>
-                                        <input type="radio" id="rating0" name="rating" value="0" /><label htmlFor="rating0" title="No star"></label>
-                                    </fieldset>
-
-                            </div>
+                            <Rating
+                                empty="mdi mdi-star-outline "
+                                full="mdi mdi-star active-star"
+                                fractions={2}
+                                
+                            />
                         </div>
                         <div className="comments">
                             {/*<TextInput
