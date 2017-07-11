9a7423f110da3359747ba068aad7a3750cc41686
acamogh <acamogh@gmail.com>
2017-07-11 14:28:14 +0530

    stars

--- src/components/common/CarType.jsx
+++ src/components/common/CarType.jsx
@@ -79,25 +79,7 @@ class CardType extends Component {
 
 
     render() {
-        const style = {
-            popupPos: {
-                left: this.state.statusPopupPosition
-            }, popupArrow: {
-                left: this.state.statusPopupArrow
-            }
-        }
         const { cardDetails, jobLeftGridValue, jobRightGridValue } = this.props;
-        const stepCount = map(cardDetails.statusPopup, (stepVal, key) => {
-            return (
-                <div className={cardDetails.statusPopup.length == key + 1 ? "bs-wizard-step active" : "bs-wizard-step complete"} key={key} >
-                    <div className="text-center bs-wizard-stepnum">Step 1</div>
-                    <div className="progress">
-                        <div className="progress-bar"></div>
-                    </div>
-                    <a className="bs-wizard-dot" onClick={(e) => { this.stepClick(e, key, cardDetails.statusPopup.length) }}></a>
-                </div>
-            )
-        })
         return (
             <div className={"job-updates " + cardDetails.statusIndicator}>
                 <div className="row">
@@ -199,6 +181,24 @@ class CardType extends Component {
                             <h4>Jaidah Automotive</h4>
                             <span className="sub-text">Basic Car Wash</span>
                             <span className="sub-text">120 AED</span>
+                        </div>
+                        <div className="stars">
+                                <div className="star-container ">
+                                    <fieldset className="rate">
+                                        <input type="radio" id="rating10" name="rating" value="10" /><label htmlFor="rating10" title="5 stars"></label>
+                                        <input type="radio" id="rating9" name="rating" value="9" /><label className="half" htmlFor="rating9" title="4 1/2 stars"></label>
+                                        <input type="radio" id="rating8" name="rating" value="8" /><label htmlFor="rating8" title="4 stars"></label>
+                                        <input type="radio" id="rating7" name="rating" value="7" /><label className="half" htmlFor="rating7" title="3 1/2 stars"></label>
+                                        <input type="radio" id="rating6" name="rating" value="6" /><label htmlFor="rating6" title="3 stars"></label>
+                                        <input type="radio" id="rating5" name="rating" value="5" /><label className="half" htmlFor="rating5" title="2 1/2 stars"></label>
+                                        <input type="radio" id="rating4" name="rating" value="4" /><label htmlFor="rating4" title="2 stars"></label>
+                                        <input type="radio" id="rating3" name="rating" value="3" /><label className="half" htmlFor="rating3" title="1 1/2 stars"></label>
+                                        <input type="radio" id="rating2" name="rating" value="2" /><label htmlFor="rating2" title="1 star"></label>
+                                        <input type="radio" id="rating1" name="rating" value="1" /><label className="half" htmlFor="rating1" title="1/2 star"></label>
+                                        <input type="radio" id="rating0" name="rating" value="0" /><label htmlFor="rating0" title="No star"></label>
+                                    </fieldset>
+
+                            </div>
                         </div>
                         <div className="comments">
                             {/*<TextInput
