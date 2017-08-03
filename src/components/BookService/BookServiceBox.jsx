02a01b11f16dc33e329f74b0865fa66236febf5a
Arun Prasad <arunprasad@Aruns-MacBook-Pro.local>
2017-08-03 16:13:01 +0530

    image dummy placeholder added

--- src/components/BookService/BookServiceBox.jsx
+++ src/components/BookService/BookServiceBox.jsx
@@ -45,7 +45,7 @@ class BookServiceBox extends Component {
                 </li>
             );
         });
-        const carPhoto = this.props.images && this.props.images.length && this.props.images[0] ? this.props.images[0].original : '/images/car.jpg';
+        const carPhoto = this.props.images && this.props.images.length && this.props.images[0] ? this.props.images[0].original : '/images/Car-Placeholder.jpg';
         return (
             <div className="col-md-4 col-sm-6 col-xs-12">
                 <div className="myCar-card " onClick={(e) => {e.preventDefault(); this.props.router.push(`/car-profiles/${this.props.id}/view`)}}>
