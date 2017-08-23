d6b517daed9a606d01422c181f86c18d7b3c056f
Bala Subramani <bala@cartoonmango.com>
2017-08-23 15:35:31 +0530

    read more bug fixed

--- src/components/MyRequest/AcceptedQuotes.jsx
+++ src/components/MyRequest/AcceptedQuotes.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Button from '../common/Button';
 import CustomScroll from 'react-custom-scroll';
 import { Media } from 'react-bootstrap';
+import Dotdotdot from 'react-dotdotdot';
 import { map, each, includes } from 'lodash';
 
 export default class AcceptedQuotes extends Component {
@@ -9,8 +10,10 @@ export default class AcceptedQuotes extends Component {
     super();
     this.state={
       imagesLeft:false,
-      imagesRemaining:0
+      imagesRemaining:0,
+      readMore:true
     }
+
       this.AcceptedQuotes = [
         {
           vdImage: '../../images/car.jpg',
@@ -56,7 +59,7 @@ export default class AcceptedQuotes extends Component {
                   photo :'../../images/test.jpg',
               }
           ],
-          PhotoDesc :'Lorem ipsum dolor sit amet, vim aperiam assentior moderatius an, eum facilisi pericula ea. Pro ut abhorreant intellegam, at est tota el..',
+          PhotoDesc :'Lorem ipsum dolor sit amet, vim aperiam assentior moderatius an, eum facilisi pericula ea. Pro ut abhorreant intellegam, at est tota el Lorem ipsum dolor sit amet, vim aperiam assentior moderatius an, eum facilisi pericula ea. Pro ut abhorreant intellegam, at est tota el',
         }
       ];
     }
@@ -67,7 +70,13 @@ export default class AcceptedQuotes extends Component {
         this.setState({imagesRemaining:imagesRemaining})
       }
     }
-
+    showText(val){
+      if(val=="more"){
+      this.setState({readMore:false})
+      }else{
+      this.setState({readMore:true})
+      }
+    }
   render() {
 
     const AcceptedQuotesList = map(this.AcceptedQuotes, (item, key) => {
@@ -98,7 +107,7 @@ export default class AcceptedQuotes extends Component {
 
             </div>
             <div className="figure no-top-padding">
-                <h4>Photos</h4>
+              <h4>Photos</h4>
               <div className="photos-row">
                 {map(item.vdPhoto, (item, key) => {
                   return(
@@ -109,12 +118,17 @@ export default class AcceptedQuotes extends Component {
                   );
                 })}
               </div>
-               {this.state.imagesLeft && <div className="upload-images wrap-photo" key={key}>
+                {this.state.imagesLeft && <div className="upload-images wrap-photo" key={key}>
                       <img src="../../images/test.jpg" alt=""/>
                       <span className="more-photos">{"+ "+this.state.imagesRemaining+" more"}</span>
                     </div>}
-                <p>{item.PhotoDesc}</p>
-                <label className="read_more">Read More</label>
+                <p>
+                  <Dotdotdot clamp={this.state.readMore?2:1000}>
+                    {item.PhotoDesc}
+                  </Dotdotdot>
+                </p>
+                {this.state.readMore && <label className="read_more" onClick={this.showText.bind(this,'more')}>Read More</label>}
+                {!this.state.readMore &&<label className="read_more" onClick={this.showText.bind(this,'less')}>Read Less</label>}
             </div>
         </div>
       );
